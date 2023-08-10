8b59163d6a03228a65ded0df737f64a839d73173
conio11 <conio416@naver.com>
2023-08-10 16:11:46 +0900

    [오다빈] vo 생성

--- JoinTree/src/main/java/com/goodee/JoinTree/vo/AnnualLeave.java
+++ JoinTree/src/main/java/com/goodee/JoinTree/vo/AnnualLeave.java
@@ -4,5 +4,12 @@ import lombok.Data;
 
 @Data
 public class AnnualLeave {
-
+	private int annualNo;
+	private int empNo;
+	private double annualCount;
+	private String annualDate;
+	private String createdate;
+	private String updatedate;
+	private int createId;
+	private int updateId;
 }
