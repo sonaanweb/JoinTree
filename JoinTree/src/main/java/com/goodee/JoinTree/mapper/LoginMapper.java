448486cd8d10a213776cf670d57f827b7f3dfb2a
conio11 <conio416@naver.com>
2023-08-16 18:38:19 +0900

    [오다빈] 로그인 mapper, service 생성 (로그인, 사원명, 사번/주민번호 체크, 비번 재설정)

---
+++ JoinTree/src/main/java/com/goodee/JoinTree/mapper/LoginMapper.java
@@ -0,0 +1,21 @@
+package com.goodee.JoinTree.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.goodee.JoinTree.vo.AccountList;
+import com.goodee.JoinTree.vo.EmpInfo;
+
+@Mapper
+public interface LoginMapper {
+	// 로그인 액션
+	AccountList selectAccount(AccountList account);
+	
+	// 로그인 성공 시 empNo로 empName 출력
+	String selectEmpName(int empNo);
+	
+	// 사번, 주민등록번호 뒷자리 체크
+	int selectEmpNoJumin(int empNo, String juminNo);
+	
+	// 비밀번호 재설정 (분실)
+	int modifyForgetPw(int empNo, String newPw);
+}
---
+++ JoinTree/src/main/java/com/goodee/JoinTree/service/LoginService.java
@@ -0,0 +1,49 @@
+package com.goodee.JoinTree.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.goodee.JoinTree.mapper.LoginMapper;
+import com.goodee.JoinTree.vo.AccountList;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@Transactional
+public class LoginService {
+	static final String CYAN = "\u001B[46m";
+	static final String RESET = "\u001B[0m";
+	
+	@Autowired
+	private LoginMapper loginMapper;
+	
+	// 로그인
+	public AccountList login(AccountList account) {
+		AccountList accountEmp = loginMapper.selectAccount(account);
+		return accountEmp;
+	}
+	
+	// 로그인 성공 시 empNo로 empName 출력
+	public String getEmpName(int empNo) {
+		return loginMapper.selectEmpName(empNo);
+	}
+	
+	// 사원, 주민번호 뒷자리 일치 체크 (비밀번호 분실 시)
+	public int selectEmpNoJumin(int empNo, String juminNo) {
+		int row = loginMapper.selectEmpNoJumin(empNo, juminNo);
+		log.debug(CYAN + row + " <-- row(LoginService-selectEmpNoJumin)" + RESET);
+		
+		return row;
+	}
+	
+	// 비밀번호 변경 (비밀번호 분실 시)
+	public int modifyForgetPw(int empNo, String newPw) {
+		int row = loginMapper.modifyForgetPw(empNo, newPw);
+		log.debug(CYAN + row + " <-- row(LoginService-modifyForgetPw)" + RESET);
+		
+		return row;
+	}
+	
+}
