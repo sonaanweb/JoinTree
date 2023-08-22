1ecc9da34ba74eec22ba585cfb1dc42995b860a4
huiju0502 <yhj960502@naver.com>
2023-08-22 14:10:13 +0900

    [양희주] 문서결재 mapper추가

--- JoinTree/src/main/java/com/goodee/JoinTree/mapper/DocumentMapper.java
+++ JoinTree/src/main/java/com/goodee/JoinTree/mapper/DocumentMapper.java
@@ -6,15 +6,69 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.goodee.JoinTree.vo.CommonCode;
 import com.goodee.JoinTree.vo.DocumentDefault;
+import com.goodee.JoinTree.vo.DocumentLeave;
+import com.goodee.JoinTree.vo.DocumentReshuffle;
+import com.goodee.JoinTree.vo.DocumentResign;
 
 @Mapper
 public interface DocumentMapper {
-		// 기본 기안서 입력
-		int insertDocDefault(DocumentDefault documentDefault);
-		
-		// 기본 기안서 상세보기
-		DocumentDefault selectDocDefaultOne(int docDefaultNo);
-		
-		// 문서결제양식 코드 조회
-		List<CommonCode> selectDocumentCodeList();
+	
+	// 기안서 입력
+	// 기본 기안서 입력
+	int addDocDefault(DocumentDefault documentDefault);
+	
+	// 휴가 기안서 입력
+	int addDocLeave(DocumentLeave documentLeave);
+	
+	// 인사이동 기안서 입력
+	int addDocReshuffle(DocumentReshuffle documentReshuffle);
+			
+	// 퇴직 기안서 입력
+	int addDocResign(DocumentResign documentResign);
+	
+	
+	// 기안서 수정
+	// 기본 기안서 수정
+	int modifyDocDefault(DocumentDefault documentDefault);
+	
+	// 휴가 기안서 수정
+	int modifyDocLeave(DocumentLeave documentLeave);
+	
+	// 인사이동 기안서 수정
+	int modifyDocReshuffle(DocumentReshuffle documentReshuffle);
+			
+	// 퇴직 기안서 수정
+	int modifyDocResign(DocumentResign documentResign);
+	
+	
+	// 기안서 삭제
+	// 기본 기안서 삭제
+	int removeDocDefault(DocumentDefault documentDefault);
+	
+	// 휴가 기안서 삭제
+	int removeDocLeave(DocumentLeave documentLeave);
+	
+	// 인사이동 기안서 삭제
+	int removeDocReshuffle(DocumentReshuffle documentReshuffle);
+			
+	// 퇴직 기안서 삭제
+	int removeDocResign(DocumentResign documentResign);
+	
+	
+	// 기안서 상세보기 
+	// 기본 기안서 상세보기
+	DocumentDefault selectDocDefaultOne(int docNo);
+	
+	// 휴가 기안서 상세보기
+	DocumentLeave selectDocLeaveOne(int docNo);
+	
+	// 인사이동 기안서 상세보기
+	DocumentReshuffle selectDocReshuffleOne(int docNo);
+	
+	// 퇴직 기안서 상세보기
+	DocumentResign selectDocResignOne(int docNo);
+			
+	
+	// 문서결제양식 코드 조회
+	List<CommonCode> selectDocumentCodeList();
 }
