aaedd729da6da3eefc44834e1b23057ee8ae2103
conio11 <conio416@naver.com>
2023-08-27 01:16:12 +0900

    [오다빈] 세션 확인 필터-홈, 커뮤니티, 코드, 나의 정보, 사원관리, 문서결재 페이지 접근 시 로그인 확인(임시)

--- JoinTree/src/main/java/com/goodee/JoinTree/JoinTreeApplication.java
+++ JoinTree/src/main/java/com/goodee/JoinTree/JoinTreeApplication.java
@@ -2,12 +2,13 @@ package com.goodee.JoinTree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-public class JoinTreeApplication {
-
+@ServletComponentScan // 서블릿, 필터, 리스너를 스캔하여 등록
+public class JoinTreeApplication { 	
 	public static void main(String[] args) {
 		SpringApplication.run(JoinTreeApplication.class, args);
+	
 	}
-
 }
--- JoinTree/src/main/java/com/goodee/JoinTree/SessionFilter.java
+++ JoinTree/src/main/java/com/goodee/JoinTree/SessionFilter.java
@@ -1,6 +1,8 @@
 package com.goodee.JoinTree;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -12,28 +14,40 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.goodee.JoinTree.vo.AccountList;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@WebFilter("/*")
+@WebFilter(urlPatterns = {"/home", "/community/*", "/code/*", "/empInfo/*", "/empManage/*", "/document/*"})
 public class SessionFilter extends HttpFilter implements Filter {
-   
-	private static final long serialVersionUID = 1L;
-
+	static final String CYAN = "\u001B[46m";
+	static final String RESET = "\u001B[0m";
+    // "/home", "/community/*", "/code/*", "/empInfo/*", "/commute/*", "/empManage/*", "/document/*", "/reservation/*", "/schedule/*"
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
+	  
+        System.out.println("세션 확인 필터");
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI(); // requestURI 변수 할당
+        // System.out.println("Context Path: " + httpRequest.getContextPath()); // 컨텍스트 경로 확인
 
         // 세션 가져오기
         HttpSession session = httpRequest.getSession();
-        log.debug("로그인 성공");
-        System.out.println("로그인성공");
+        Object loginAccount = session.getAttribute("loginAccount");
 
-        if(session.getAttribute("loginAccount") == null || session.getAttribute("loginAccount").equals("")) {
-        	httpResponse.sendRedirect(httpRequest.getContextPath() + "/login/login");
-			return;
-		}
-		
-		chain.doFilter(request, response);
-	}
+        String msg = "";
+        if (!requestURI.endsWith("/login") && (loginAccount == null || loginAccount.equals(""))) {
+            msg = URLEncoder.encode("로그인 후 이용 가능합니다.", "UTF-8");
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/login/login?msg=" + msg);
+            return;
+        }
+
+        httpRequest.setAttribute("loginAccount", loginAccount);
+
+        log.debug(CYAN + "로그인 상태 -> empNo: " + ((AccountList) loginAccount).getEmpNo() + RESET);
+        // System.out.println("로그인성공");
+
+        chain.doFilter(request, response);
+    }
 }
